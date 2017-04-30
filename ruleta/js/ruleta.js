0c063bfb82899216074d6f57fc8b8c03703a9f90
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-04-29 21:57:57 -0300

    closes #29

--- ruleta/js/ruleta.js
+++ ruleta/js/ruleta.js
@@ -2,19 +2,49 @@
 
 alert('Bienvenido al juego de la ruleta!!!');
 
-var costo=10;
-var saldo=100;
-var numeroApuesta= -1;
-var numeroRuleta=-1;
-var salio=-1;
-var selecciono=-1;
+let apuesta0 = document.getElementById('0');
+apuesta0.addEventListener("click",function(){selectNro(0);});
+
+let apuesta1 = document.getElementById('1');
+apuesta1.addEventListener("click",function(){selectNro(1);});
+
+let apuesta2 = document.getElementById('2');
+apuesta2.addEventListener("click",function(){selectNro(2);});
+
+let apuesta3 = document.getElementById('3');
+apuesta3.addEventListener("click",function(){selectNro(3);});
+
+let apuesta4 = document.getElementById('4');
+apuesta4.addEventListener("click",function(){selectNro(4);});
+
+let apuesta5 = document.getElementById('5');
+apuesta5.addEventListener("click",function(){selectNro(5);});
+
+let apuesta6 = document.getElementById('6');
+apuesta6.addEventListener("click",function(){selectNro(9);});
+
+let apuesta7 = document.getElementById('7');
+apuesta7.addEventListener("click",function(){selectNro(7);});
+
+let apuesta8 = document.getElementById('8');
+apuesta8.addEventListener("click",function(){selectNro(8);});
+
+let apuesta9= document.getElementById('9');
+apuesta9.addEventListener("click",function(){selectNro(9);});
+
+let costo=10;
+let saldo=100;
+let numeroApuesta= -1;
+let numeroRuleta=-1;
+let salio=-1;
+let selecciono=-1;
 
 document.getElementById('saldo').innerHTML='$' + saldo;
 document.getElementById('numero').value="";
 
 function selectNro(nro){
   document.getElementById(nro).style.background="#17A600";
-  ganancia=2*nro;
+  let ganancia=2*nro;
   if (selecciono!=-1){
     document.getElementById(selecciono).style.background="white";
   }
