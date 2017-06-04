cec6cf16eab9037972f5d64fddfd1733b45b2f86
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-06-04 17:08:32 -0300

    Se arreglo el problema del estilo en el home

--- CPSsengundaEntrega/views/view_home.php
+++ CPSsengundaEntrega/views/view_home.php
@@ -4,12 +4,15 @@ require_once('libs/Smarty.class.php');
 
 class viewHome{
    private $smarty;
+   private $baseDir;
 
   public function __construct(){
     $this->smarty = new smarty();
+    $this->baseDir = 'http://'.$_SERVER['SERVER_NAME'] . dirname($_SERVER['PHP_SELF']).'/';
   }
 
   public function mostrar(){
+    $this->smarty->assign("baseDir", $this->baseDir);
     $this->smarty->display('home.tpl');
   }
 }
