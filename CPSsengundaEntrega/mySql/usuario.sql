06f191b032dc5232b3028d6c0ce258d1663ae958
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-06-10 20:13:00 -0300

    Se arreglo un script mysql y se borro otro

--- CPSsengundaEntrega/mySql/usuario.sql
+++ CPSsengundaEntrega/mySql/usuario.sql
@@ -1,10 +1,7 @@
 CREATE TABLE `usuario` (
   `id_usuario` int(11) NOT NULL,
-  `nombre` varchar(100) COLLATE utf8_bin NOT NULL,
-  `apellido` varchar(100) COLLATE utf8_bin NOT NULL,
-  `email` varchar(200) COLLATE utf8_bin NOT NULL,
-  `telefono` int(30) NOT NULL,
-  `descripcion` text COLLATE utf8_bin NOT NULL,
+  `password` varchar(62) COLLATE utf8_bin NOT NULL,
+  `email` varchar(50) COLLATE utf8_bin NOT NULL,
   `admin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
@@ -12,4 +9,4 @@ ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id_usuario`);
 
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;COMMIT;  
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
