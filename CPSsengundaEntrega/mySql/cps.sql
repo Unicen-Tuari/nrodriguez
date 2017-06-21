7741033f472ac3cc7db050c550dc65b13acb936c
NicoARodriguez <nrodriguezbeaulocq@gmail.com>
2017-06-21 17:06:20 -0300

    Se agregaron insert

--- CPSsengundaEntrega/mySql/cps.sql
+++ CPSsengundaEntrega/mySql/cps.sql
@@ -46,6 +46,10 @@ ALTER TABLE `cps`.`categoria`
   )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 
-  ALTER TABLE `cps`.`producto` ADD
-  CONSTRAINT fk_categoria
-  FOREIGN KEY (id_categoria) REFERENCES cps.categoria(id_categoria);
+ALTER TABLE `cps`.`producto` ADD
+CONSTRAINT fk_categoria
+FOREIGN KEY (id_categoria) REFERENCES cps.categoria(id_categoria);
+
+INSERT INTO `cps`. `producto` (`nombre`, `precio`, `marca`, `color`, `display`, `procesador`, `camaraPrincipal`, `camaraSecundaria`, `sistemaOp`, `red`, `frecuencia`, `bateria`, `tiempo`, `memoriaI`, `peso`, `dimension`, `pantalla`, `bluetooth`, `marcaporVoz`, `id_categoria`)
+VALUES ('Iphone 7 plus 128GB\r\n', '$30.999', 'Apple', 'Blanco', '4.7\' HD LED IPS', 'A10-Chip A10 Fusión con arquitectura de 64 bits\r\n', '12 mpx | Zoom digital 5x\r\n', '7 mpx', 'IOS 10', 'GSM, 3G, 4G LTE', 'GSM 850/900/1800/1900 MHz | WCDMA 850/900/1700-2100/1900/2100 MHz\r\n', '10 Dias', '14 HS', '128 GB', '138 g', '138,3 x 67,1 x 7,1 mm', 'Touchscreem', '1', '1', '1'),
+('LG G5+Camara 360° y VR\r\n', '$13.499', 'LG', 'Plateado', '5.3\'\' IPS', 'Quad Core 2.0 GHz + Quad Core 1.6 GHz\r\n', '16 mpx con flash LED | Zoom digital 4x\r\n', '8 mpx', 'Android 6.0', 'GSM, 3G, 4G LTE', 'GSM 850/900/1800/1900 MHz | WCDMA 850/900/1700/1900/2100 MHz\r\n', '8 días', '12 h', '32 GB ', '159 g', '149,4 x 73,9 x 7,3 mm', 'Touchscreem', '1', '1', '40');
